711bd1c4c58a0bef877526ac5eb6be53dd8149fe
dinithib <mailtodinithi@gmail.com>
2021-08-19 10:02:12 +1000

    local_branchtest

--- app/src/main/java/com/example/testbr/MainActivity.kt
+++ app/src/main/java/com/example/testbr/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        print("Hello")
         //This ia common change for all
         print("Common change test")
     }
