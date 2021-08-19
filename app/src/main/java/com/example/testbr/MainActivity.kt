31201090cad3cb46e1f21a88a35a82672b2f85ec
dinithib <mailtodinithi@gmail.com>
2021-08-19 10:42:12 +1000

    common change to master

--- app/src/main/java/com/example/testbr/MainActivity.kt
+++ app/src/main/java/com/example/testbr/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //This ia common change for all
+        print("Common change test")
     }
 }
